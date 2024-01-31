777efc1e3d1211b423b7b7e5ab620166b417d7f0
soonyoong.chia <soonyoong.chia@hytechc.com>
2024-01-31 18:39:26 +0800

    continue

---
+++ src/main/java/com/example/jwtagain/JwtAgainApplication.java
@@ -0,0 +1,13 @@
+package com.example.jwtagain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtAgainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtAgainApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jwtagain/JwtAgainApplicationTests.java
@@ -0,0 +1,15 @@
+package com.example.jwtagain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtAgainApplicationTests {
+
+	@Test
+	void contextLoads() {
+		System.out.println("aaa");
+
+	}
+
+}
